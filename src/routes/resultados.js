827b9fbd31a67c5320bd0ee7019cf6135e9151b2
Felipe Amoroso Lambaz <felipe.lambaz@sptech.school>
2026-05-19 21:44:58 -0300

    adiciona a rota ultimo

--- src/routes/resultados.js
+++ src/routes/resultados.js
@@ -4,5 +4,6 @@ var resultadoController = require("../controllers/resultadosController");
 
 router.post("/salvar", resultadoController.salvar);
 router.get("/listar/:idUsuario", resultadoController.listar);
+router.get("/ultimo/:idUsuario", resultadoController.ultimo);
 
 module.exports = router;
