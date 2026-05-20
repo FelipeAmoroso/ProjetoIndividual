3c63096a6175db70eb260c0588c6d4a22d8ad4c1
Felipe Amoroso Lambaz <felipe.lambaz@sptech.school>
2026-05-19 21:44:00 -0300

    adiciona função ultimo

--- src/models/resultadosModel.js
+++ src/models/resultadosModel.js
@@ -1,19 +1,15 @@
 var database = require("../database/config");
 
 function salvar(fkUsuario, fkTentativa, acertos, erros) {
-
     var instrucao = `
         INSERT INTO resultados (fkUsuario, fkTentativa, acertos, erros, dataHora)
         VALUES (${fkUsuario}, ${fkTentativa}, ${acertos}, ${erros}, NOW());
     `;
-
     console.log("SQL:", instrucao);
-
     return database.executar(instrucao);
 }
 
 function listar(idUsuario) {
-
     var instrucao = `
         SELECT 
             u.nome,
@@ -26,13 +22,27 @@ function listar(idUsuario) {
         WHERE u.idUsuario = ${idUsuario}
         ORDER BY t.data_tentativa DESC;
     `;
-
     console.log("SQL:", instrucao);
+    return database.executar(instrucao);
+}
 
+function ultimo(idUsuario) {
+    var instrucao = `
+        SELECT 
+            r.acertos,
+            r.erros
+        FROM resultados r
+        JOIN tentativas t ON r.fkTentativa = t.idTentativa
+        WHERE r.fkUsuario = ${idUsuario}
+        ORDER BY t.data_tentativa DESC
+        LIMIT 1;
+    `;
+    console.log("SQL:", instrucao);
     return database.executar(instrucao);
 }
 
 module.exports = {
     salvar,
-    listar
+    listar,
+    ultimo
 };
