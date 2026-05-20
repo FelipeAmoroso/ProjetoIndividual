679c9b87ea71542fe6c65ba2b839f8a60aff71fc
Felipe Amoroso Lambaz <felipe.lambaz@sptech.school>
2026-05-19 21:42:45 -0300

    corrige validação de campos e adiciona função ultimo

--- src/controllers/resultadosController.js
+++ src/controllers/resultadosController.js
@@ -1,22 +1,19 @@
 var resultadoModel = require("../models/resultadosModel");
 
 function salvar(req, res) {
+
+    if (req.body.fkUsuario == undefined || req.body.fkTentativa == undefined ||
+        req.body.acertos == undefined || req.body.erros == undefined) {
+        res.status(400).send("Dados inválidos");
+        return;
+    }
+
     var fkUsuario = Number(req.body.fkUsuario);
     var fkTentativa = Number(req.body.fkTentativa);
     var acertos = Number(req.body.acertos);
     var erros = Number(req.body.erros);
 
-    console.log("Dados recebidos:", {
-        fkUsuario,
-        fkTentativa,
-        acertos,
-        erros
-    });
-
-    if (fkUsuario == undefined || fkTentativa == undefined || acertos == undefined || erros == undefined) {
-        res.status(400).send("Dados inválidos");
-        return;
-    }
+    console.log("Dados recebidos:", { fkUsuario, fkTentativa, acertos, erros });
 
     resultadoModel.salvar(fkUsuario, fkTentativa, acertos, erros)
         .then(resultado => res.json(resultado))
@@ -33,7 +30,15 @@ function listar(req, res) {
         .catch(erro => res.status(500).json(erro));
 }
 
+function ultimo(req, res) {
+    var idUsuario = req.params.idUsuario;
+    resultadoModel.ultimo(idUsuario)
+        .then(resultado => res.json(resultado))
+        .catch(erro => res.status(500).json(erro));
+}
+
 module.exports = {
     salvar,
-    listar
+    listar,
+    ultimo
 };
